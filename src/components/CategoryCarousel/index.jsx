6b9e58f62ff72e804c6a3422c2be815a3ecc68af
giuseppebarbetta <giuseppebarbetta@outlook.com>
2024-05-13 23:35:13 -0300

    feat: start create carousels

--- src/components/CategoryCarousel/index.jsx
+++ src/components/CategoryCarousel/index.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Carousel from 'react-elastic-carousel';
 import { apiDevPizza as api } from '../../services/api';
 import * as C from './style';
 
 function CategoryCarousel() {
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     async function loadCategories() {
-      const response = await api.get('categories');
+      const { data } = await api.get('categories');
 
-      console.log(response);
+      setCategories(data);
     }
     loadCategories();
   }, []);
@@ -15,6 +18,15 @@ function CategoryCarousel() {
   return (
     <C.Container>
       <C.Title>CATEGORIAS</C.Title>
+      <Carousel itemsToShow={4}>
+        {categories &&
+          categories.map((category) => (
+            <div key={category.id}>
+              <img src={category.url} alt="foto da categoria" />
+              <button>{category.name}</button>
+            </div>
+          ))}
+      </Carousel>
     </C.Container>
   );
 }
