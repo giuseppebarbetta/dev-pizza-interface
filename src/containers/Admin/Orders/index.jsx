0ba6e69363e32c133479633843f2391e45a5fb59
giuseppebarbetta <giuseppebarbetta@outlook.com>
2024-06-06 22:53:32 -0300

    feat: init config admin page

---
+++ src/containers/Admin/Orders/index.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { apiDevPizza as api } from '../../../services/api';
+import * as C from './style';
+
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+function Orders() {
+  const [orders, setOrders] = useState([]);
+  const [rows, setRows] = useState([]);
+  console.log(orders);
+
+  useEffect(() => {
+    async function loadOrders() {
+      const { data } = await api.get('orders');
+
+      setOrders(data);
+    }
+
+    loadOrders();
+  }, []);
+
+  function createData(order) {
+    return {
+      name: order.user.name,
+      orderId: order._id,
+      date: order.createdAt,
+      status: order.status,
+      products: order.products,
+    };
+  }
+
+  useEffect(() => {
+    const newRows = orders.map((order) => createData(order));
+    setRows(newRows);
+  }, [orders]);
+
+  return (
+    <C.Container>
+      <h1>Pedidos</h1>
+    </C.Container>
+  );
+}
+
+export default Orders;
--- src/containers/Admin/index.jsx
+++ src/containers/Admin/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import * as C from './style';
+import Orders from './Orders';
 
 export function Admin() {
   return (
     <C.Container>
-      <h1>Eu sou Admin</h1>
+      <Orders />
     </C.Container>
   );
 }
